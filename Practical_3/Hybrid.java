46664b4af137364e1860f4d07962c77f4a2967e9
Davitree <davidkhadka444@gmail.com>
2026-05-26 15:45:36 +0545

    Hybrid inheritance sample program

---
+++ Practical_3/Hybrid.java
@@ -0,0 +1,23 @@
+package Practical_3;
+
+class Ant{
+  void ant(){
+    System.out.println("ant is an insect");
+  }
+}
+class Bat extends Ant{
+  void bat(){
+    System.out.println("bat is a mammal");
+  }
+}
+class Cat extends Bat {
+
+}
+public class Hybrid {
+  public static void main(String[] args) {
+    Bat b = new Bat();
+    b.ant();
+    Cat c = new Cat();
+    c.bat();
+  }
+}
