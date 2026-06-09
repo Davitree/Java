6877b0d338a32264cd53c8b593c42781af3974f4
Davitree <davidkhadka444@gmail.com>
2026-06-09 14:40:19 +0545

    First swing program

---
+++ Practical_5/Swing.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+public class Swing{
+  public static void main(String[] args) {
+      JFrame f = new JFrame("Components and Container");
+      JButton btn = new JButton("Click me.");
+      f.add(btn);
+      f.setSize(300,200);
+      f.setLayout(null);
+      btn.setBounds(100,100,100,100);
+      f.setVisible(true);
+      f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  }
+}
