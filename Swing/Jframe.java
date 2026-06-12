be5b86ac404c7bc33a8071e83254b0fe81731bf6
Davitree <davidkhadka444@gmail.com>
2026-06-12 21:16:06 +0545

    learning swing

---
+++ Swing/Jframe.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+public class Jframe{
+ public static void main(String[] args) {
+  JFrame f = new JFrame();
+  f.setSize(500,500);
+  f.setVisible(true);
+  f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ } 
+}
